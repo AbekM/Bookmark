fd36217afa8dfdd2a911257a653712e020d542f1
AbekM <asilbek.makhmudov@outlook.com>
2019-07-10 18:55:59 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+document.getElementById("defaultOpen").click();
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "flex";
+    evt.currentTarget.className += " active";
+} 
+
+
+// ACCORDION
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle('activex');
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+            
+        } else {
+            panel.style.display = "block";
+        }
+    });
+} 
+
+// EMAIL VALIDATION
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+let validateBtn = document.getElementById('validate');
+
+function validate() {
+    var result = document.getElementById('result');
+    var email = document.getElementById('email').value;
+    // $result.text("");
+
+    if (validateEmail(email)) {
+        result.innerHTML = "Thanks for choosing us!";
+        result.style.backgroundColor = "#29a265";
+        document.body.classList.add('green');
+
+    } else {
+        result.innerHTML = "Whoops, make sure it is an email";
+        result.style.backgroundColor = "#fb5859"
+        document.body.classList.add('red');
+
+    }
+    return false;
+}
+
+validateBtn.addEventListener('click', validate);
